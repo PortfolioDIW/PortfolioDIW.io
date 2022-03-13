67d8b98d771f753ad7ffd9b042cb26b52d19a63d
Thinh Nguyen - Martin <41408547+MartinNguyen0801@users.noreply.github.com>
2022-03-14 01:34:33 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+/*function realtimeclock(){
+	var rtClock= new Date();
+	
+	var hours= rtClock.getHours();
+	var minutes= rtClock.getMinutes();
+	var seconds= rtClock.getSeconds();
+	
+	var amPM= (hour < 12)? "AM":"PM";
+	
+	hours= (hours > 12)? (hours-12):hours;
+	
+	hours= ("0" + hours).slice(-2);
+	minutes= ("0" + minutes).slice(-2);
+	seconds= ("0" + seconds).slice(-2);
+	
+	document.getElementById('clock').innerHTML=
+		hours + "  :  " + minutes + "  :  " + seconds + " " + amPM;
+	var t= setTimeout(realtimeclock, 500);
+	
+	
+}*/
+
+
+
+
+
+
